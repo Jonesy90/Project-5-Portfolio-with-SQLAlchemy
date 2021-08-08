ce87b595eeb69e291c270d8c51eb370b3f8f788d
Michael Jones <michael.jones90@me.com>
2021-08-08 20:01:48 +0100

    General fix -- date fix.

--- app.py
+++ app.py
@@ -12,17 +12,21 @@ def index():
     projects = Project.query.all()
     return render_template('index.html', projects=projects)
 
-
+#A route to the 'create project' page.
+#Takes the values supplied by user in the form and creates a Project object, which is then sent to the database.
 @app.route('/new', methods=['GET', 'POST'])
 def create():
     if request.form:
-        new_project = Project(title=request.form['title'], date=datetime.datetime.strptime(request.form['date'], '%d/%m/%Y'), description=request.form['desc'], skills=request.form.get['skills'], project_link=request.form['github'])
+        new_project = Project(title=request.form['title'], date=datetime.datetime.strptime(request.form['date'], '%d/%m/%Y'), description=request.form['desc'], skills=request.form['skills'], project_link=request.form['github'])
         db.session.add(new_project)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('projectform.html')
 
 
+#A route to the 'detail' page.
+#When the user selects a project, they are taken to a new page which details everything about that project for them to view.
+#On this page, the user can decide to update or delete the project.
 @app.route('/detail/<id>')
 def detail(id):
     project = Project.query.get(id)
@@ -31,6 +35,8 @@ def detail(id):
     return render_template('detail.html', project=project, skills=skills, projects=projects)
 
 
+#A route to allow the user to 'edit' the project they're current under.
+#This has an identical form as the 'create' page but it will override any all the values.
 @app.route('/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
     project = Project.query.get(id)
